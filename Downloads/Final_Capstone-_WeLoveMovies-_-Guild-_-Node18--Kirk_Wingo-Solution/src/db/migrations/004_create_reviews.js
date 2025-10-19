be74c5049af7c76ea5dc4cc8b3b1ad6e57fbe0c7
Kirk Wingo <50303717+kwingo@users.noreply.github.com>
2025-10-19 09:05:13 -0400

    Refactor migration for reviews table syntax

--- Downloads/Final_Capstone-_WeLoveMovies-_-Guild-_-Node18--Kirk_Wingo-Solution/src/db/migrations/004_create_reviews.js
+++ Downloads/Final_Capstone-_WeLoveMovies-_-Guild-_-Node18--Kirk_Wingo-Solution/src/db/migrations/004_create_reviews.js
@@ -1,26 +1,13 @@
-module.exports = {
-  up: function (knex) {
-    return knex.schema.createTable("reviews", (t) => {
-      t.increments("review_id").primary();
-      t.text("content");
-      t.integer("score");
-      t
-        .integer("critic_id")
-        .unsigned()
-        .references("critic_id")
-        .inTable("critics")
-        .onDelete("CASCADE");
-      t
-        .integer("movie_id")
-        .unsigned()
-        .references("movie_id")
-        .inTable("movies")
-        .onDelete("CASCADE");
-      t.timestamps(true, true);
-    });
-  },
+exports.up = (knex) =>
+  knex.schema.createTable("reviews", (t) => {
+    t.increments("review_id").primary();
+    t.text("content");
+    t.integer("score");
+    t.integer("critic_id").unsigned()
+      .references("critic_id").inTable("critics").onDelete("CASCADE");
+    t.integer("movie_id").unsigned()
+      .references("movie_id").inTable("movies").onDelete("CASCADE");
+    t.timestamps(true, true);
+  });
 
-  down: function (knex) {
-    return knex.schema.dropTableIfExists("reviews");
-  },
-};
+exports.down = (knex) => knex.schema.dropTableIfExists("reviews");
