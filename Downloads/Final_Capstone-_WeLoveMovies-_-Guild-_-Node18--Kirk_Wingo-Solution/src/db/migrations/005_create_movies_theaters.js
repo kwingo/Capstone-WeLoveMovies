ab6144af3f87b6d8b79c1c5aec696539d7f8a882
Kirk Wingo <50303717+kwingo@users.noreply.github.com>
2025-10-19 09:09:05 -0400

    Refactor movie_id and theater_id references

--- Downloads/Final_Capstone-_WeLoveMovies-_-Guild-_-Node18--Kirk_Wingo-Solution/src/db/migrations/005_create_movies_theaters.js
+++ Downloads/Final_Capstone-_WeLoveMovies-_-Guild-_-Node18--Kirk_Wingo-Solution/src/db/migrations/005_create_movies_theaters.js
@@ -1,9 +1,19 @@
 exports.up = (knex) =>
   knex.schema.createTable("movies_theaters", (t) => {
-    t.integer("movie_id").unsigned()
-      .references("movie_id").inTable("movies").onDelete("CASCADE");
-    t.integer("theater_id").unsigned()
-      .references("theater_id").inTable("theaters").onDelete("CASCADE");
+    t
+      .integer("movie_id")
+      .unsigned()
+      .references("movie_id")
+      .inTable("movies")
+      .onDelete("CASCADE");
+
+    t
+      .integer("theater_id")
+      .unsigned()
+      .references("theater_id")
+      .inTable("theaters")
+      .onDelete("CASCADE");
+
     t.boolean("is_showing").defaultTo(false);
     t.timestamps(true, true);
     t.primary(["movie_id", "theater_id"]);
