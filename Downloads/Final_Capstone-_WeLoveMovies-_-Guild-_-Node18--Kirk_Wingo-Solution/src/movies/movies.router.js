65ee132b98bc5e73147f1de35938ecb263419606
Kirk Wingo <50303717+kwingo@users.noreply.github.com>
2025-10-19 13:48:00 -0400

    Refactor movie router to use middleware for reviews and theaters

--- Downloads/Final_Capstone-_WeLoveMovies-_-Guild-_-Node18--Kirk_Wingo-Solution/src/movies/movies.router.js
+++ Downloads/Final_Capstone-_WeLoveMovies-_-Guild-_-Node18--Kirk_Wingo-Solution/src/movies/movies.router.js
@@ -1,7 +1,12 @@
+const router = require("express").Router({ mergeParams: true });
+
 const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 const reviewsRouter = require("../reviews/reviews.router");
 const theatersRouter = require("../theaters/theaters.router");
+
+router.use("/:movieId/reviews", controller.movieExists, reviewsRouter);
+router.use("/:movieId/theaters", controller.movieExists, theatersRouter);
 
 router
   .route("/")
@@ -12,8 +17,5 @@ router
   .route("/:movieId")
   .get(controller.read)
   .all(methodNotAllowed);
-
-router.use("/:movieId/reviews", controller.movieExists, reviewsRouter);
-router.use("/:movieId/theaters", controller.movieExists, theatersRouter);
 
 module.exports = router;
