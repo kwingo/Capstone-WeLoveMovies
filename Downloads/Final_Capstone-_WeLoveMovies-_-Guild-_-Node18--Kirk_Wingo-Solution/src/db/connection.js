a4f321c0adf469ca91eeac5da70dd6deb5234c1e
Kirk Wingo <50303717+kwingo@users.noreply.github.com>
2025-10-19 13:32:00 -0400

    Refactor config loading in connection.js

--- Downloads/Final_Capstone-_WeLoveMovies-_-Guild-_-Node18--Kirk_Wingo-Solution/src/db/connection.js
+++ Downloads/Final_Capstone-_WeLoveMovies-_-Guild-_-Node18--Kirk_Wingo-Solution/src/db/connection.js
@@ -1,4 +1,4 @@
 const environment = process.env.NODE_ENV || "development";
-const config = require("../../knexfile")[environment];
+const config = require("../../knexfile")[process.env.NODE_ENV || "development"];
 const knex = require("knex")(config);
 module.exports = knex;
