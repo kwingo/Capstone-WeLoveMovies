d7968f915cc1f0b7b07914f4d4a9b0218f01fcdb
Kirk Wingo <50303717+kwingo@users.noreply.github.com>
2025-10-19 13:48:25 -0400

    Refactor environment variable handling in connection.js

--- Downloads/Final_Capstone-_WeLoveMovies-_-Guild-_-Node18--Kirk_Wingo-Solution/src/db/connection.js
+++ Downloads/Final_Capstone-_WeLoveMovies-_-Guild-_-Node18--Kirk_Wingo-Solution/src/db/connection.js
@@ -1,4 +1,4 @@
-const environment = process.env.NODE_ENV || "development";
-const config = require("../../knexfile")[process.env.NODE_ENV || "development"];
+const env = process.env.NODE_ENV || "development";
+const config = require("../../knexfile")[env];
 const knex = require("knex")(config);
 module.exports = knex;
